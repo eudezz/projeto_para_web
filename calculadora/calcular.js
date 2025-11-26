b419bd00c10c8be4767fff4e514543c4abffc731
eudez0 <eudeszanizete@gmail.com>
2025-11-26 19:20:11 -0300

    Criando pasta calcular

---
+++ calculadora/calcular.js
@@ -0,0 +1,12 @@
+
+
+
+function calculu() {
+    var p = window.document.getElementById("peso")
+    var al= window.document.getElementById("altura")
+    var pe = Number (p.velue)
+    var alt=Number(al.value)
+    var resp =window.document.getElementById ("resp")
+     var resp
+
+ }
