b579af24152a2f0fbe6faebb4058ce75fdf1307f
eudez0 <eudeszanizete@gmail.com>
2025-12-02 17:26:22 -0300

    Criado site relogio

---
+++ relogio/relogio.js
@@ -0,0 +1,17 @@
+function digital(){
+    var relogio = document.getElementById('relogio')
+    var data= new Date ()
+    var hora= data.getHours()
+    var minuto = data.getMinutes()
+    var segundo= data.getSeconds()
+    var rel= document.getElementById("rel")
+    hora = hora < 10 ? '0' + hora : hora;
+    minuto = minuto < 10 ? '0' + minuto : minuto;
+    segundo = segundo < 10 ? '0' + segundo : segundo;
+    rel.innerHTML= `${hora}: ${minuto} : ${segundo}`
+    relogio.style.fontSize = '2em'
+    relogio.style.textAlign='center'
+    
+    
+    
+}
